40a2aa64fa8183e3efdfd6024987ec5737111ca1
Ajit <ajitdhayal3157@gmail.com>
2024-08-22 17:44:18 +0530

    Added Chain of Responsibility design pattern

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import ChainOfResponsibility.Debugger;
+import ChainOfResponsibility.ErrorLog;
+import ChainOfResponsibility.information;
+import ChainOfResponsibility.logger;
+
 public class Main {
     public static void main(String[] args) {
 
+        logger lg = new information(new Debugger(new ErrorLog(null)));
+        lg.log("error" , "this is custom error");
+        lg.log("info" , "this is custom info");
+        lg.log("debug" , "this is custom debug");
     }
 }
